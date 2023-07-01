369e195e27446faacc0180ce82bcfd8f0189c10d
maciuso <maciek.otto.pl@gmail.com>
2023-07-01 14:33:03 +0200

    1.07.23

---
+++ fileanalist.py
@@ -0,0 +1,14 @@
+import csv
+
+titles = {}
+
+with open("favourites.csv", "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+      title = row["title"].strip().upper()
+      if not title in titles:
+        titles[title] = 0
+      titles[title] += 1 
+
+for title in sorted(titles, lambda title: titles[title], reverse = True):
+  print(title, titles[title])
