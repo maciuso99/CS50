ccc0a21da2f57a2fdde3d392cdada1af0c741d4e
maciuso <maciek.otto.pl@gmail.com>
2023-07-09 21:05:10 +0200

    9.07.23

--- fileanalist.py
+++ fileanalist.py
@@ -2,13 +2,14 @@ import csv
 
 titles = {}
 
-with open("favourites.csv", "r") as file:
+with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
-      title = row["title"].strip().upper()
+      title = row["language"].strip().upper()
       if not title in titles:
         titles[title] = 0
       titles[title] += 1 
 
-for title in sorted(titles, lambda title: titles[title], reverse = True):
+for title in sorted(titles, key=lambda title: titles[title], reverse = True):
   print(title, titles[title])
+ 
